dd2d921a2eea40d9640a3444ac44a382c1c2c934
Vago Aleksandr <vgoalex666@mail.ru>
2022-06-19 17:00:30 +0300

    feat: full adaptive from >2000 and to 1400 is over

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../Header/Header';
-import { Characters, Comics, Page404, SingleComicPage } from '../pages';
+import LoadingSinglePage from '../pages/singleComic/loadingSinglePage/LoadingSinglePage';
 
 import './App.scss';
+
+const Characters = lazy(() => import('../pages/characters/Characters'));
+const Comics = lazy(() => import('../pages/comics/Comics'));
+const Page404 = lazy(() => import('../pages/page404/Page404'));
+const SingleComicPage = lazy(() =>
+  import('../pages/singleComic/SingleComicPage')
+);
 
 const path = '/marvelApp__React';
 
@@ -14,15 +22,17 @@ const App = () => {
         <Header />
       </header>
       <main className='main'>
-        <Routes>
-          <Route path={`${path}/`} element={<Characters />} />
-          <Route path={`${path}/comics`} element={<Comics />} />
-          <Route
-            path={`${path}/comics/:comicId`}
-            element={<SingleComicPage />}
-          />
-          <Route path='*' element={<Page404 />} />
-        </Routes>
+        <Suspense fullback={<LoadingSinglePage />}>
+          <Routes>
+            <Route path={`${path}/`} element={<Characters />} />
+            <Route path={`${path}/comics`} element={<Comics />} />
+            <Route
+              path={`${path}/comics/:comicId`}
+              element={<SingleComicPage />}
+            />
+            <Route path='*' element={<Page404 />} />
+          </Routes>
+        </Suspense>
       </main>
     </Router>
   );
--- src/components/ComicsBanner/ComicsBanner.js
+++ src/components/ComicsBanner/ComicsBanner.js
@@ -1,27 +1,27 @@
-import { useState } from 'react';
-
 import './ComicsBanner.scss';
 
 const ComicsBanner = () => {
   return (
-    <section className='Comics wrapper'>
-      <div className='Comics-box'>
-        <div className='Comics-box-logo'>
+    <section className='ComicsBanner wrapper'>
+      <div className='ComicsBanner-box'>
+        <div className='ComicsBanner-box-logo'>
           <img
             src='https://i.giphy.com/media/9r75CL4HcusetkC3d4/giphy.webp'
             alt='logo-stanLee'
-            className='Comics-box-logo__img'
+            className='ComicsBanner-box-logo__img'
           />
         </div>
-        <div className='Comics-box-titles'>
-          <h3 className='Comics-box-titles__title'>New comics every week!</h3>
-          <h3 className='Comics-box-titles__title'>Stay tuned!</h3>
+        <div className='ComicsBanner-box-titles'>
+          <h3 className='ComicsBanner-box-titles__title'>
+            New comics every week!
+          </h3>
+          <h3 className='ComicsBanner-box-titles__title'>Stay tuned!</h3>
         </div>
-        <div className='Comics-box-logo'>
+        <div className='ComicsBanner-box-logo'>
           <img
             src='https://i.giphy.com/media/13kajTax0GCg0g/giphy.webp'
             alt='logo-Avengers'
-            className='Comics-box__logo-avengers'
+            className='ComicsBanner-box__logo-avengers'
           />
         </div>
       </div>
